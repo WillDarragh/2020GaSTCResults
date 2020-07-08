60cbfbec1b9f354a3bbee61bbf686be589ce81eb
willdarragh <wdarragh16@gmail.com>
2020-07-07 22:19:59 -0400

    Added piechart for missing counties

--- main.py
+++ main.py
@@ -7,9 +7,20 @@ import numpy as np
 import pandas as pd
 import json
 
+# Setup pie chart
+
+pie_df = pd.read_csv(r'C:\Users\wdarr\PycharmProjects\2020GaSTCResults\data\missing_counties.dat')
+
+print(pie_df)
+
+pie = px.pie(pie_df, values='Winners', names='Region', title='Winners of Regions in GA (Count of winners by county to '
+                                                             'low to map)')
+
+#pie.show()
+
 # Setup map
 
-with open(r'C:\Users\wdarr\PycharmProjects\2020GaSTCResults\data\merged_ga_counties.json') as counties_file:
+with open(r'C:\Users\wdarr\PycharmProjects\2020GaSTCResults\data\ga_counties.json') as counties_file:
     counties = json.load(counties_file)
 
 df = pd.read_csv(r'C:\Users\wdarr\PycharmProjects\2020GaSTCResults\data\winners_by_county.csv',
@@ -28,13 +39,15 @@ fig = px.choropleth(df,
                     )
 fig.update_geos(fitbounds="locations", visible=False)
 fig.update_layout(margin={"r": 0, "t": 0, "l": 0, "b": 0})
-fig.show()
+#fig.show()
 
 # BEGIN DASH APP
 app = dash.Dash()
 
 app.layout = html.Div([
-    dcc.Graph(figure=fig)
+    html.H1('2020 GaSTC Number of Winners by County'),
+    dcc.Graph(figure=fig),
+    dcc.Graph(figure=pie)
 ])
 # END DASH APP
 
